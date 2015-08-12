d72b48dcfae1ee3d37e77fefe7223c5f7ffa8a2c
RCollins13 <Ryan Collins>
2015-08-11 22:21:13 -0400

    Add module 7

---
+++ scripts/module7.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#liWGS-SV Pipeline: Module 7 (Complex SV Categorization)
+#August 2015
+#Contact: rcollins@chgr.mgh.harvard.edu
+
+#Read input
+samples_list=$1
+params=$2
+
+#Source params file
+. ${params}
+
+#Dynamically determine min complex size
+minCXsize=$( fgrep -v "#" ${OUTDIR}/QC/cohort/${COHORT_ID}.QC.metrics | cut -f9 | sort -nrk1,1 | head -n1 )
+
+#Make cnMOPS list
+while read ID bam sex; do
+	echo -e "${ID}\t${WRKDIR}\t${ID}\t${ID}.cnMOPS.dels.bed" > ${WRKDIR}/classifier/clusterfix/newCoords/cnMOPS_dels.list
+	echo -e "${ID}\t${WRKDIR}\t${ID}\t${ID}.cnMOPS.dups.bed" > ${WRKDIR}/classifier/clusterfix/newCoords/cnMOPS_dups.list
+done < ${samples_list}
+
+#Submit inversion categorization
+bsub -q normal -sla miket_sc -o ${OUTDIR}/logs/inversion_categorization.log -e ${OUTDIR}/logs/inversion_categorization.log -J ${COHORT_ID}_classification "${liWGS_SV}/scripts/classify_inversion.sh ${WRKDIR}/classifier/clusterfix/newCoords/inversion.events.reclassified.bedpe ${WRKDIR}/classifier/clusterfix/${COHORT_ID}_inversion.patched.clusters ${minCXsize} ${WRKDIR}/classifier/clusterfix/newCoords/deletion.events.reclassified.bedpe ${WRKDIR}/classifier/clusterfix/newCoords/insertion.events.reclassified.bedpe ${WRKDIR}/classifier/clusterfix/newCoords/cnMOPS_dels.list ${WRKDIR}/classifier/clusterfix/newCoords/cnMOPS_dups.list ${WRKDIR}/classifier/clusterfix/newCoords/"
+
+#Submit translocation categorization
+bsub -q normal -sla miket_sc -o ${OUTDIR}/logs/inversion_categorization.log -e ${OUTDIR}/logs/inversion_categorization.log -J ${COHORT_ID}_classification "${liWGS_SV}/scripts/classify_translocation.sh ${WRKDIR}/classifier/clusterfix/newCoords/transloc.events.reclassified.bedpe ${WRKDIR}/classifier/clusterfix/${COHORT_ID}_transloc.patched.clusters ${WRKDIR}/classifier/clusterfix/newCoords/"
+
+#Submit complex linking script
+
