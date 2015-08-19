a8b51e34d11aeca6bebd862e0d5d0d4d70d88ecf
RCollins13 <Ryan Collins>
2015-08-19 12:39:43 -0400

    Clean up size plotting

--- scripts/scrapeSVsizes.R
+++ scripts/scrapeSVsizes.R
@@ -19,10 +19,10 @@ cpx <- as.vector(read.table(paste(args[1],"complex.size",sep=""),header=F)[,1])
 densities <- lapply(list(del,dup,inv,ins_src,cpx),function(vals){return(density(log10(vals)))})
 
 #Calculate summary
-res <- as.data.frame(t(matrix(unlist(lapply(list(del,dup,inv,ins_src,cpx),summary)),nrow=6)))
-rownames(res) <- c("Deletion","Duplication","Inversion","Insertion (Source)","Complex (Approx.)")
-colnames(res) <- c("Min","Q1","Med","Mean","Q3","Max")
-write.table(res,args[2],sep="\t",append=T,row.names=T,col.names=T,quote=F)
+res <- data.frame(c("Deletion","Duplication","Inversion","Insertion (Source)","Complex (Approx.)"),
+                  t(matrix(unlist(lapply(list(del,dup,inv,ins_src,cpx),summary)),nrow=6)))
+colnames(res) <- c("Class","Min","Q1","Med","Mean","Q3","Max")
+write.table(res,args[2],sep="\t",append=T,row.names=F,col.names=T,quote=F)
 
 #Plot
 colors <- c("firebrick","dodgerblue","darkorange","darkorchid4","aquamarine")
@@ -33,6 +33,11 @@ plot(0,0,type="n",
      xaxs="i",yaxs="i",xaxt="n",
      ylab="Density",xlab="Variant Size",main="Variant Sizes by Class")
 grid(nx=NULL,ny=NA,col="black",lty=2)
+abline(v=log10(c(seq(2000,9000,by=1000),
+                 seq(20000,90000,by=10000),
+                 seq(200000,900000,by=100000),
+                 seq(2000000,9000000,by=1000000))),
+       col="gray20")
 for(i in 1:5){
   polygon(x=c(densities[[i]]$x,rev(densities[[i]]$x)),
           y=c(densities[[i]]$y,rep(0,length(densities[[i]]$y))),
@@ -45,5 +50,6 @@ legend("topright",
                 "Simple Inversion",
                 "Insertion",
                 "Complex (Approx.)"),
-       col=colors,lwd=3)
+       col=colors,lwd=3,
+       bg="white")
 dev.off()
